236b0e3abbaa7ab898bf3ab591e758632f506044
Martin <martinchidozie27@gmail.com>
2026-03-20 13:15:10 +0100

    fix sw cache killing api routes

--- components/PWARegister.tsx
+++ components/PWARegister.tsx
@@ -4,12 +4,21 @@ import { useEffect } from 'react'
 
 export default function PWARegister() {
   useEffect(() => {
-    if ('serviceWorker' in navigator) {
+    if (!('serviceWorker' in navigator)) return
+
+    // First unregister ALL existing service workers to clear any stale cache
+    navigator.serviceWorker.getRegistrations().then((registrations) => {
+      for (const registration of registrations) {
+        registration.unregister()
+        console.log('SW unregistered:', registration.scope)
+      }
+    }).then(() => {
+      // Then register fresh
       navigator.serviceWorker
         .register('/sw.js')
-        .then(() => console.log('SW registered'))
+        .then(() => console.log('SW registered fresh'))
         .catch(err => console.log('SW error', err))
-    }
+    })
   }, [])
 
   return null
